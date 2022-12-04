1ad0bf47ce859ff210549d048d2fb05ef035bf73
jzilio16 <114541860+jzilio16@users.noreply.github.com>
2022-12-04 10:46:05 -0300

    initial commit

---
+++ LAB-Supermarket/src/main/java/br/com/LABSupermarket/LAB/Supermarket/LabSupermarketApplication.java
@@ -0,0 +1,13 @@
+package br.com.LABSupermarket.LAB.Supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSupermarketApplication.class, args);
+	}
+
+}
---
+++ LAB-Supermarket/src/test/java/br/com/LABSupermarket/LAB/Supermarket/LabSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.LABSupermarket.LAB.Supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
